a6f288d38846c2c46d1df63f250009e1afbcdb0a
qaushqestaj <qaushqestaj@hotmail.com>
2023-05-31 13:21:48 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '74bf3a92-7f94-4fcb-b98b-6315d7ea23cf';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,45 @@
+import View from './View';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class addRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _message = 'Recipe was successfully uploaded';
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _buttonOpen = document.querySelector('.nav__btn--add-recipe');
+  _buttonClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindows();
+    this._addHandlerHideWindow();
+  }
+
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindows() {
+    this._buttonOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._buttonClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr);
+      handler(data);
+    });
+  }
+
+  _generateMarkup() {}
+}
+
+export default new addRecipeView();
